e0411263983c6cca3171b3b193468a22da0bdad8
Pranav <pranavmaheshwari123@gmail.com>
2023-02-05 21:24:56 +0530

    Header

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import './Header.css'
 function Header(){
+    let Images_Array=["https://thumbs.dreamstime.com/b/female-doctor-smiling-background-patient-bed-two-doctors-173319504.jpg","https://sgmh.org/wp-content/uploads/2017/11/1.jpg"];
     return(
         <div className="header container-fluid">
             <div className="header_box row">
+            
                 {/* <div className="col-lg-6">
             {/* <img src="https://purepng.com/public/uploads/large/purepng.com-doctorsdoctorsdoctors-and-nursesa-qualified-practitioner-of-medicine-aclinicianmedical-practitionermale-doctornotepad-1421526856940m4nhi.png" alt="Hospital Image"></img> */}
 
