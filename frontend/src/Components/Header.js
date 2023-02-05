abe9fae3a92f655ab5deff26bccfbd32fb9d006d
Pranav <pranavmaheshwari123@gmail.com>
2023-02-05 17:01:50 +0530

    Added Navbar

---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg bg-dark">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">Care Digital</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav">
+                                <a className="nav-link" href="#">About</a>
+                                <a className="nav-link" href="#">Doctors</a>
+                                <a className="nav-link" href="#">Book Appointment</a>
+                                <a className="nav-link" href="#">Contact Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Header
