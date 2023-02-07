ff0f5cea36cc5d8c46a32f841299136f640b877d
Pranav <pranavmaheshwari123@gmail.com>
2023-02-07 17:16:27 +0530

    Change Doctors to Review Component

--- frontend/src/Components/ImageSlider/SLider.js
+++ frontend/src/Components/ImageSlider/SLider.js
@@ -2,78 +2,76 @@ import React, { Component } from 'react'
 import "./Slider.css"
 
 
-let DoctorList = [
+let PatientList = [
     {
         id: 1,
-        Name: "xyz",
+        Name: "Rahul Sharma",
         Link: "https://www.hiranandanihospital.org/public/our_doctors_images/1661326951.jpg",
-        Department: "abc"
+        Review: "I have found amazing result with the advance technique"
     },
     {
         id: 2,
-        Name: "pqr",
+        Name: "Naveen Purohit",
         Link: "https://www.hiranandanihospital.org/public/our_doctors_images/1661359438.jpg",
-        Department: "abc"
+        Review: "It was a great experience with Doctors . 5 years back I was so worried about my hair loss but now my problem is cured"
     },
     {
         id: 3,
-        Name: "qwerty",
-        Link: "https://www.hiranandanihospital.org/public/our_doctors_images/1661362617.jpg",
-        Department: "abc"
+        Name: "Jagdish Khanna",
+        Link: "https://www.hiranandanihospital.org/public/our_doctors_images/1661359438.jpg",
+        Review: "It was a great experience with Doctors . 5 years back I was so worried about my hair loss but now my problem is cured"
     }
 ]
 class SLider extends Component {
     constructor(props) {
         super(props)
 
-        this.state = DoctorList[0]
+        this.state = PatientList[0]
     }
     // Binding Using Arrow function
     nextImage = () => {
         let Current=this.state.id;
-        // console.log(Current,DoctorList[Current-1],DoctorList.indexOf({id:Current}));
+        // console.log(Current,PatientList[Current-1],PatientList.indexOf({id:Current}));
 
-        if(Current!=DoctorList.length){
-            this.setState(DoctorList[Current])
+        if(Current!=PatientList.length){
+            this.setState(PatientList[Current])
         }
         else{
-            this.setState(DoctorList[0])
+            this.setState(PatientList[0])
         }
 
     }
     prevImage = () => {
         let Current=this.state.id;
-        // console.log(Current,DoctorList[Current-2]);
+        // console.log(Current,PatientList[Current-2]);
         if(Current!=1){
-            this.setState(DoctorList[Current-2])
+            this.setState(PatientList[Current-2])
         }
         else{
-            this.setState(DoctorList[DoctorList.length-1])
+            this.setState(PatientList[PatientList.length-1])
 
         }
 
     }
     render() {
         return (
-            <div className='Doctor_List container-fluid'>
-                <h2 className='text-center my-3'>Our Doctors</h2>
+            <div className='Doctor_List container mt-5'>
+                <h2 className='text-center my-3'>Patient Reviews</h2>
                 <div className='row'>
-                    <div className='col-sm-2'>
+                    <div className='col-sm-2 d-flex align-items-center'>
                     <button className='btn btn-danger' onClick={this.nextImage}>Next</button>
                     </div>
                 <div className='card DoctorCard col-auto'>
                     <div className='card-body' key={this.state.id}>
                     <img src={this.state.Link}></img>
-                    <h2>{this.state.Name}</h2>
-                    <h5>{this.state.Department}</h5>
+                    <h2 className='my-2'>{this.state.Name}</h2>
+                    <h6 className='fst-italic my-1'>{`" ${this.state.Review} "`}</h6>
+                    <img src='https://tse4.mm.bing.net/th?id=OIP.QOhwh1JLNOHV6U0URqQpAAHaCt&pid=Api&P=0'></img>
                     </div>
-                    <div className='card-footer'>
-                        <a href=''>Book Appointment</a>
-
-                    </div>
+                    
 
                 </div>
-                <div className='col-sm-2'>
+                <div className='col-sm-2 d-flex align-items-center offset-sm-1'>
                 <button className='btn btn-success' onClick={this.prevImage}>Pervious</button>
 
                     </div>
